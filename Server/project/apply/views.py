6bdc4150e28de9a1ad657d0091e1c72524301a5d
sky712345678 <sky712345678@hotmail.com>
2019-06-07 00:48:27 +0800

    minor adjustments.

--- Server/project/apply/views.py
+++ Server/project/apply/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from .models import Case
 from check.models import CaseFiles
 from authentication.views import group_required
+from django.views.decorators.csrf import csrf_exempt
 
 import json
 import os
@@ -16,13 +17,15 @@ today = ""
 count = 0
 
 
-@group_required('House', 'Volunteer')
+# @group_required('House', 'Volunteer')
+# @csrf_exempt
 def home(request):
     path = os.path.abspath('.') + "/templates/apply.html"
     return render(request, path)
 
 
-@group_required('House', 'Volunteer')
+# @group_required('House', 'Volunteer')
+# @csrf_exempt
 def upload(request):
     address = nullInputHandle(request.POST.get("county"), 1) + nullInputHandle(request.POST.get("district"), 1) + nullInputHandle(request.POST.get("road"), 1) + nullInputHandle(request.POST.get("section"), 1) + nullInputHandle(request.POST.get("lane"), 1) + nullInputHandle(request.POST.get("alley"), 1) + nullInputHandle(request.POST.get("number"), 1) + nullInputHandle(request.POST.get("numberD"), 1) + nullInputHandle(request.POST.get("floor"), 1) + nullInputHandle(request.POST.get("floorD"), 1) + nullInputHandle(request.POST.get("room"), 1)
 
